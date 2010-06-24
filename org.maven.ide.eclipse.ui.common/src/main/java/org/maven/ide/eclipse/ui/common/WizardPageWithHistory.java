48cf01386c005aad6d7deada11fa30965fd586c9
Milos <mkleint@sonatype.com>
2010-06-24 13:01:09 +0200

    copy wizardpagewithhistory to the eclipse-commons repo

---
+++ org.maven.ide.eclipse.ui.common/src/main/java/org/maven/ide/eclipse/ui/common/WizardPageWithHistory.java
@@ -0,0 +1,64 @@
+package org.maven.ide.eclipse.ui.common;
+
+import java.beans.Beans;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Combo;
+import org.maven.ide.eclipse.ui.common.InputHistory;
+
+abstract public class WizardPageWithHistory
+    extends WizardPage
+{
+    private InputHistory inputHistory;
+
+    /** a helper flag to ensure the history is only loaded once */
+    private boolean historyLoaded = false;
+
+    protected WizardPageWithHistory( String title )
+    {
+        super( title );
+        setTitle( title );
+
+    }
+
+    /** Loads the advanced settings data when the page is displayed. */
+    public void setVisible( boolean visible )
+    {
+        if ( visible  && !Beans.isDesignTime())
+        {
+            if ( !historyLoaded )
+            {
+                getInputHistory().load();
+                historyLoaded = true;
+            }
+            else
+            {
+                getInputHistory().save();
+            }
+        
+        }
+        super.setVisible( visible );
+    }
+
+    /** Saves the history when the page is disposed. */
+    public void dispose()
+    {
+        if (!java.beans.Beans.isDesignTime()) {
+            getInputHistory().save();
+        }
+        super.dispose();
+    }
+
+    protected void addToInputHistory( String id, Combo combo )
+    {
+        getInputHistory().add( id, combo );
+    }
+
+    protected InputHistory getInputHistory()
+    {
+        if (inputHistory == null) {
+            inputHistory = new InputHistory( getTitle() );
+        }
+        return inputHistory;
+    }
+}
