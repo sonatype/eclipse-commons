174a3871f14cd2711fa8d32539d9c75f94c0bd8b
dplatonoff <dmitry@platonoff.com>
2010-08-05 14:49:05 -0400

    MECLIPSE-1378: realm composite

---
+++ org.maven.ide.eclipse.ui.common/src/main/java/org/maven/ide/eclipse/ui/common/authentication/RealmComposite.java
@@ -0,0 +1,263 @@
+package org.maven.ide.eclipse.ui.common.authentication;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.maven.ide.eclipse.authentication.AnonymousAccessType;
+import org.maven.ide.eclipse.authentication.AuthFacade;
+import org.maven.ide.eclipse.authentication.AuthRegistryException;
+import org.maven.ide.eclipse.authentication.IAuthRealm;
+import org.maven.ide.eclipse.swtvalidation.SwtComponentDecorationFactory;
+import org.maven.ide.eclipse.swtvalidation.SwtValidationGroup;
+import org.maven.ide.eclipse.ui.common.composites.DropDownComposite;
+import org.netbeans.validation.api.Problems;
+import org.netbeans.validation.api.Validator;
+import org.netbeans.validation.api.ui.ValidationListenerFactory;
+import org.netbeans.validation.api.ui.ValidationStrategy;
+
+public class RealmComposite
+    extends DropDownComposite
+{
+    private Text urlText;
+
+    private List list;
+
+    private Combo combo;
+
+    private static final AnonymousAccessType[] anonymousOptions =
+        new AnonymousAccessType[] { AnonymousAccessType.NOT_ALLOWED, AnonymousAccessType.ALLOWED,
+            AnonymousAccessType.REQUIRED };
+
+    private static final String[] anonymousLabels =
+        new String[] { "Password Required", "Anonymous Allowed", "Anonymous Only" };
+
+    private boolean dirty;
+
+    private boolean updating;
+
+    private String url;
+
+    private IAuthRealm[] realms;
+
+    private IAuthRealm lastSelectedRealm;
+
+    private AnonymousAccessType anonymousAccessType = AnonymousAccessType.NOT_ALLOWED;
+
+    public RealmComposite( Composite parent, Text urlText, SwtValidationGroup validationGroup, boolean formMode )
+    {
+        super( parent, formMode ? SWT.FLAT : 0 );
+        setClient( urlText );
+        setValidationGroup( validationGroup );
+    }
+
+    private void setClient( final Text text )
+    {
+        urlText = text;
+        text.addModifyListener( new ModifyListener()
+        {
+            public void modifyText( ModifyEvent e )
+            {
+                if ( updating )
+                {
+                    return;
+                }
+
+                url = text.getText().trim();
+                if ( url.length() == 0 )
+                {
+                    setEnabled( false );
+                    setText( "" );
+                    dirty = false;
+                }
+                else
+                {
+                    IAuthRealm realm = null;
+                    try
+                    {
+                        realm = AuthFacade.getAuthRegistry().getRealmForURI( url );
+                    }
+                    catch ( AuthRegistryException authRegistryException )
+                    {
+                        // bad url, big deal...
+                    }
+
+                    if ( realm == null )
+                    {
+                        setEnabled( true );
+                        updateText();
+                    }
+                    else
+                    {
+                        setEnabled( false );
+                        setText( realm.getName() );
+                        dirty = false;
+                    }
+                }
+            }
+        } );
+    }
+
+    private void setValidationGroup( SwtValidationGroup validationGroup )
+    {
+        Validator<String> validator = new Validator<String>()
+        {
+
+            public Class<String> modelType()
+            {
+                return String.class;
+            }
+
+            public void validate( Problems problems, String componentName, String model )
+            {
+                if ( url != null && url.length() > 0 && model.length() == 0 )
+                {
+                    problems.add( NLS.bind( "Select security realm for {0}", urlText.getData( "_name" ) ) );
+                }
+            }
+        };
+
+        validationGroup.addItem(
+                                 ValidationListenerFactory.createValidationListener(
+                                                                                     getTextControl(),
+                                                                                     ValidationStrategy.DEFAULT,
+                                                                                     SwtComponentDecorationFactory.getDefault().decorationFor(
+                                                                                                                                               this ),
+                                                                                     validator ), false );
+    }
+
+    @Override
+    protected Composite createPopupContent( Shell parent )
+    {
+        parent.addShellListener( new ShellAdapter()
+        {
+            @Override
+            public void shellActivated( ShellEvent e )
+            {
+                realms = AuthFacade.getAuthRegistry().getRealms().toArray( new IAuthRealm[0] );
+                String[] names = new String[realms.length];
+                int selection = -1;
+                for ( int i = realms.length - 1; i >= 0; i-- )
+                {
+                    String name = realms[i].getName();
+                    names[i] = name;
+                    if ( lastSelectedRealm != null && name.equals( lastSelectedRealm.getName() ) )
+                    {
+                        selection = i;
+                    }
+                }
+                list.setItems( names );
+                list.setSelection( selection );
+            }
+        } );
+
+        Composite composite = new Composite( parent, SWT.BORDER );
+        composite.setLayout( new GridLayout( 2, false ) );
+        composite.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
+
+        list = new List( composite, SWT.BORDER | SWT.V_SCROLL | SWT.SINGLE );
+        GridData gd = new GridData( SWT.FILL, SWT.TOP, true, false, 2, 1 );
+        gd.heightHint = 80;
+        gd.minimumHeight = 80;
+        list.setLayoutData( gd );
+
+        Listener listener = new Listener()
+        {
+            public void handleEvent( Event event )
+            {
+                if ( event.widget == list )
+                {
+                    switch ( event.type )
+                    {
+                        case SWT.Selection:
+                            int n = list.getSelectionIndex();
+                            if ( n >= 0 )
+                            {
+                                lastSelectedRealm = realms[n];
+                                updateText();
+                                dirty = true;
+                            }
+                            break;
+                        case SWT.MouseDoubleClick:
+                            showPopup( false );
+                            break;
+                    }
+                }
+            }
+        };
+        list.addListener( SWT.MouseDoubleClick, listener );
+        list.addListener( SWT.Selection, listener );
+
+        Label label = new Label( composite, SWT.NONE );
+        label.setText( "Access:" );
+        label.setLayoutData( new GridData( SWT.LEFT, SWT.CENTER, false, false ) );
+
+        combo = new Combo( composite, SWT.NONE );
+        combo.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+        combo.setItems( anonymousLabels );
+        combo.addSelectionListener( new SelectionAdapter()
+        {
+            @Override
+            public void widgetSelected( SelectionEvent e )
+            {
+                int n = combo.getSelectionIndex();
+                if ( n >= 0 )
+                {
+                    anonymousAccessType = anonymousOptions[n];
+                }
+                updateText();
+                dirty = true;
+            }
+        } );
+        combo.select( 0 );
+
+        return composite;
+    }
+
+    private void updateText()
+    {
+        updating = true;
+        setText( lastSelectedRealm == null ? "" : lastSelectedRealm.getName() );
+        updating = false;
+    }
+
+    @Override
+    public boolean isFocusControl()
+    {
+        if ( list.isFocusControl() || combo.isFocusControl() )
+        {
+            return true;
+        }
+        return super.isFocusControl();
+    }
+
+    public boolean isDirty()
+    {
+        return dirty;
+    }
+
+    public void save( IProgressMonitor monitor )
+    {
+        if ( dirty && lastSelectedRealm != null && url != null && url.length() > 0 )
+        {
+            AuthFacade.getAuthRegistry().addURLToRealmAssoc( url, lastSelectedRealm.getId(), anonymousAccessType,
+                                                             monitor );
+        }
+        dirty = false;
+    }
+}
---
+++ org.maven.ide.eclipse.ui.common/src/main/java/org/maven/ide/eclipse/ui/common/composites/DropDownComposite.java
@@ -0,0 +1,221 @@
+package org.maven.ide.eclipse.ui.common.composites;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public abstract class DropDownComposite
+    extends Composite
+{
+    private Text text;
+
+    private Button button;
+
+    private Shell popup;
+
+    private Composite popupComposite;
+
+    public DropDownComposite( Composite parent, int style )
+    {
+        super( parent, SWT.BORDER );
+
+        GridLayout gridLayout = new GridLayout( 2, false );
+        gridLayout.horizontalSpacing = 0;
+        gridLayout.marginHeight = 0;
+        gridLayout.marginWidth = 0;
+        gridLayout.verticalSpacing = 0;
+        setLayout( gridLayout );
+
+        int flatStyle = style & SWT.FLAT;
+
+        text = new Text( this, SWT.READ_ONLY | flatStyle );
+        text.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
+        text.addKeyListener( new KeyAdapter()
+        {
+            @Override
+            public void keyPressed( KeyEvent e )
+            {
+                if ( e.keyCode == SWT.ARROW_UP || e.keyCode == SWT.ARROW_DOWN )
+                {
+                    e.doit = false;
+                    if ( ( e.stateMask & SWT.ALT ) != 0 )
+                    {
+                        togglePopup();
+                    }
+                }
+            }
+        } );
+        text.addMouseListener( new MouseAdapter()
+        {
+            @Override
+            public void mouseDown( MouseEvent e )
+            {
+                togglePopup();
+            }
+        } );
+
+        button = new Button( this, SWT.ARROW | SWT.DOWN | flatStyle );
+        button.setLayoutData( new GridData( SWT.RIGHT, SWT.FILL, false, true ) );
+        button.addSelectionListener( new SelectionAdapter()
+        {
+            @Override
+            public void widgetSelected( SelectionEvent e )
+            {
+                togglePopup();
+            }
+        } );
+
+        createPopupShell();
+    }
+
+    private void createPopupShell()
+    {
+        popup = new Shell( getShell(), SWT.NO_TRIM | SWT.ON_TOP );
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.horizontalSpacing = 0;
+        gridLayout.marginHeight = 0;
+        gridLayout.marginWidth = 0;
+        gridLayout.verticalSpacing = 0;
+        popup.setLayout( gridLayout );
+
+        Listener listener = new Listener()
+        {
+            public void handleEvent( Event event )
+            {
+                if ( event.widget == popup && popup.isVisible() )
+                {
+                    switch ( event.type )
+                    {
+                        case SWT.Close:
+                            event.doit = false;
+                        case SWT.Deactivate:
+                            showPopup( false );
+                            break;
+                    }
+                }
+            }
+        };
+        popup.addListener( SWT.Close, listener );
+        popup.addListener( SWT.Deactivate, listener );
+
+        popupComposite = createPopupContent( popup );
+    }
+
+    abstract protected Composite createPopupContent( Shell popup );
+
+    public void showPopup( boolean show )
+    {
+        if ( show )
+        {
+            if ( isVisible() )
+            {
+                Display display = getDisplay();
+                Point popupSize = popup.computeSize( SWT.DEFAULT, SWT.DEFAULT );
+                Rectangle parentRect = display.map( getParent(), null, getBounds() );
+                Point comboSize = getSize();
+                Rectangle displayRect = getMonitor().getClientArea();
+                int width = Math.max( comboSize.x, popupSize.x );
+                int height = popupSize.y;
+                int x = parentRect.x;
+                int y = parentRect.y + comboSize.y;
+                if ( y + height > displayRect.y + displayRect.height )
+                {
+                    y = parentRect.y - height;
+                }
+                if ( x + width > displayRect.x + displayRect.width )
+                {
+                    x = displayRect.x + displayRect.width - popupSize.x;
+                }
+                popup.setBounds( x, y, width, height );
+                popup.setVisible( true );
+                if ( isFocusControl() )
+                {
+                    popupComposite.setFocus();
+                }
+            }
+        }
+        else
+        {
+            if ( popup.isVisible() )
+            {
+                popup.setVisible( false );
+                if ( !popup.isDisposed() && isFocusControl() )
+                {
+                    text.setFocus();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isFocusControl()
+    {
+        if ( text.isFocusControl() || button.isFocusControl() || popup.isFocusControl() )
+        {
+            return true;
+        }
+        return super.isFocusControl();
+    }
+
+    @Override
+    public void dispose()
+    {
+        super.dispose();
+        popup.dispose();
+    }
+
+    public String getText()
+    {
+        return text.getText();
+    }
+
+    public void setText( String text )
+    {
+        this.text.setText( text );
+    }
+
+    public Text getTextControl()
+    {
+        return text;
+    }
+
+    public void addModifyListener( ModifyListener listener )
+    {
+        text.addModifyListener( listener );
+    }
+
+    public void removeModifyListener( ModifyListener listener )
+    {
+        text.removeModifyListener( listener );
+    }
+
+    @Override
+    public void setEnabled( boolean enabled )
+    {
+        super.setEnabled( enabled );
+        text.setEnabled( enabled );
+        button.setEnabled( enabled );
+        showPopup( false );
+    }
+
+    public void togglePopup()
+    {
+        showPopup( !popup.isVisible() );
+    }
+}
