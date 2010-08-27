a38921a1e9d8e8e5d3af449c56db3c5bbf2fb336
Matthew Piggott <mpiggott@sonatype.com>
2010-08-26 22:10:26 -0400

    Fixed missing file & added more tests for HttpFetcher

--- org.maven.ide.eclipse.io.test/src/org/maven/ide/eclipse/io/HttpFetcherTest.java
+++ org.maven.ide.eclipse.io.test/src/org/maven/ide/eclipse/io/HttpFetcherTest.java
@@ -1,6 +1,8 @@
 package org.maven.ide.eclipse.io;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.Map;
@@ -10,6 +12,7 @@ import junit.framework.TestCase;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jetty.http.HttpHeaders;
+import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.http.security.B64Code;
 import org.eclipse.jetty.util.StringUtil;
 import org.maven.ide.eclipse.authentication.AnonymousAccessType;
@@ -18,7 +21,6 @@ import org.maven.ide.eclipse.authentication.AuthenticationType;
 import org.maven.ide.eclipse.authentication.IAuthData;
 import org.maven.ide.eclipse.authentication.IAuthRealm;
 import org.maven.ide.eclipse.authentication.internal.AuthData;
-import org.maven.ide.eclipse.io.HttpBaseSupport.HttpInputStream;
 import org.maven.ide.eclipse.tests.common.HttpServer;
 
 public class HttpFetcherTest
@@ -38,6 +40,8 @@ public class HttpFetcherTest
         fetcher = new HttpFetcher();
         server = new HttpServer();
         server.enableRecording( new String[] { ".*" } );
+        server.addUser( "validuser", "password", "validuser" );
+        server.addSecuredRealm( "/secured/*", "validuser" );
         server.addResources( "/", "resources" );
         server.start();
     }
@@ -54,7 +58,60 @@ public class HttpFetcherTest
     }
 
     /*
-     * Tests that the authentication header contains bother a username and password.
+     * Tests the error thrown when a file is not found.
+     */
+    public void testOpenstreamFileNotFound()
+        throws Exception
+    {
+        URI address = URI.create( server.getHttpUrl() + "/nonExistentFile" );
+        try
+        {
+            readstream( fetcher.openStream( address, new NullProgressMonitor(), AuthFacade.getAuthService(), null ) );
+        }
+        catch ( NotFoundException e )
+        {
+            assertTrue( e.getMessage().contains( String.valueOf( HttpStatus.NOT_FOUND_404 ) ) );
+            assertTrue( e.getMessage().contains( HttpStatus.getMessage( HttpStatus.NOT_FOUND_404 ) ) );
+            return;
+        }
+        fail( "NotFoundException should be thrown." );
+    }
+
+    /*
+     * Tests the error thrown when a user does not have perission to access a file.
+     */
+    public void testOpenstreamForbidden()
+        throws Exception
+    {
+        URI address = URI.create( server.getHttpUrl() + "/secured/secure.txt" );
+        try
+        {
+            readstream( fetcher.openStream( address, new NullProgressMonitor(), AuthFacade.getAuthService(), null ) );
+        }
+        catch ( UnauthorizedException e )
+        {
+            assertTrue( e.getMessage().contains( String.valueOf( HttpStatus.UNAUTHORIZED_401 ) ) );
+            assertTrue( e.getMessage().contains( HttpStatus.getMessage( HttpStatus.UNAUTHORIZED_401 ) ) );
+            return;
+        }
+        fail( "UnauthorizedException should be thrown." );
+
+    }
+
+    /*
+     * Tests the contents of a file remote file
+     */
+    public void testOpenStream()
+        throws Exception
+    {
+        URI address = URI.create( server.getHttpUrl() + "/file.txt" );
+        assertEquals( readstream( new FileInputStream( "resources/file.txt" ) ),
+                     readstream( fetcher.openStream( address, new NullProgressMonitor(), AuthFacade.getAuthService(),
+                                                     null ) ) );
+    }
+
+    /*
+     * Tests that the authentication header contains both a username and password.
      */
     public void testUsernameAndPasswordSent()
         throws Exception
@@ -120,12 +177,20 @@ public class HttpFetcherTest
     /*
      * Read a stream
      */
-    private static void readstream( HttpInputStream stream )
+    private static String readstream( InputStream stream )
         throws IOException
     {
-        while ( stream.read( new byte[128] ) == 128 )
+        StringBuilder builder = new StringBuilder();
+        byte[] buffer = new byte[128];
+        int size = -1;
+        while ( ( size = stream.read( buffer ) ) == 128 )
         {
+            builder.append( new String( buffer, 0, size ) );
         }
+        stream.close();
+        if ( size != -1 )
+            builder.append( new String( buffer, 0, size ) );
+        return builder.toString();
     }
 
     /*
