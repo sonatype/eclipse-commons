a8de48006a41890085486897ef4cdcc22ce2559c
dplatonoff <dmitry@platonoff.com>
2010-10-19 23:52:28 -0400

    MECLIPSE-1872: timeout test added

---
+++ org.maven.ide.eclipse.io.test/src/org/maven/ide/eclipse/io/TimeoutTest.java
@@ -0,0 +1,46 @@
+package org.maven.ide.eclipse.io;
+
+import java.io.IOException;
+
+import org.maven.ide.eclipse.tests.common.HttpServer;
+
+public class TimeoutTest
+    extends AbstractIOTest
+{
+    protected void startHttpServer( long latency )
+        throws Exception
+    {
+        server = new HttpServer();
+        server.addResources( "/", "resources" );
+        server.setLatency( latency );
+        server.start();
+    }
+
+    public void testNoTimeout()
+        throws Exception
+    {
+        tryHead( 28000 ); // 28 seconds should be fine (timeout is 30)
+    }
+
+    public void testTimeout()
+        throws Exception
+    {
+        try
+        {
+            tryHead( 32000 ); // 32 seconds should be too much (timeout is 30)
+        }
+        catch ( IOException e )
+        {
+            assertEquals( "Request is expected to time out",
+                          "The server did not respond within the configured timeout", e.getMessage() );
+        }
+    }
+
+    private void tryHead( long latency )
+        throws Exception
+    {
+        startHttpServer( latency );
+        S2IOFacade.head( server.getHttpUrl() + "/file.txt", null, monitor );
+        server.stop();
+    }
+}
